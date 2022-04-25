78f011648064e25249eea3c16e9d4262d4980902
NidhishaPahade <101486006+NidhishaPahade@users.noreply.github.com>
2022-04-25 23:03:18 +1000

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,51 @@
-var express = require("express")
-var app = express()
+let express = require("express");
+let app = express();
+let dbConnect = require("./dbConnect");
 
-app.use(express.static(__dirname+'/public'))
+//dbConnect.dbConnect()
+//var app = require('express')();
+let http = require('http').createServer(app);
+let io = require('socket.io')(http);
+//const MongoClient = require('mongodb').MongoClient;
+
+// routes
+let projectsRoute = require('./routes/projects')
+
+
+var port = process.env.PORT || 8080;
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.static(__dirname + '/public'));
+app.use('/api/projects',projectsRoute)
 
-const addNumbers = (number1, number2) => {
-    var num1 = parseInt(number1)
-    var num2 = parseInt(number2)
-    var result = num1 + num2;
-    return result;
-}
 
-app.get("/addTwoNumbers",(req,res) => {
-    var number1 = req.query.number1;
-    var number2 = req.query.number2;
-    var result = addNumbers(number1,number2)
-    res.json({statusCode: 200, data: result, message:"Success"})
+app.get("/test", function (request, response) {
+  var user_name = request.query.user_name;
+  response.end("Hello " + user_name + "!");
+});
+
+app.get('/addTwoNumbers/:firstNumber/:secondNumber', function(req,res,next){
+  var firstNumber = parseInt(req.params.firstNumber) 
+  var secondNumber = parseInt(req.params.secondNumber)
+  var result = firstNumber + secondNumber || null
+  if(result == null) {
+    res.json({result: result, statusCode: 400}).status(400)
+  }
+  else { res.json({result: result, statusCode: 200}).status(200) } 
 })
 
-var port = process.env.port || 3000;
+//socket test
+io.on('connection', (socket) => {
+  console.log('a user connected', socket.id);
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+  setInterval(()=>{
+    socket.emit('number', parseInt(Math.random()*10));
+  }, 1000);
 
-app.listen(port,()=>{
-    console.log("App running at http://localhost:"+port)
-})
+});
+
+
+http.listen(port,()=>{
+  console.log("Listening on port ", port);
+});
