29ae75eabcffadcc402623d8963e5e85c94b3a3e
NidhishaPahade <101486006+NidhishaPahade@users.noreply.github.com>
2022-04-25 22:43:17 +1000

    Update adding.js

--- adding.js
+++ adding.js
@@ -1,15 +1,26 @@
-const express = require('express');
-const app = express();
+var express = require("express")
+var app = express()
 
-app.get('/sum',function(req,res){
-    var a=Number(req.query.first);
-    var b=Number(req.query.sec);
-    var c;
-    c=a+b;
-    response = {
-        result: c  
-    };
-    console.log(response);
-    res.end(JSON.stringify(response));
+app.use(express.static(__dirname+'/public'))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const addNumbers = (number1, number2) => {
+    var num1 = parseInt(number1)
+    var num2 = parseInt(number2)
+    var result = num1 + num2;
+    return result;
 }
-);
+
+app.get("/addTwoNumbers",(req,res) => {
+    var number1 = req.query.number1;
+    var number2 = req.query.number2;
+    var result = addNumbers(number1,number2)
+    res.json({statusCode: 200, data: result, message:"Success"})
+})
+
+var port = process.env.port || 3000;
+
+app.listen(port,()=>{
+    console.log("App running at http://localhost:"+port)
+})
